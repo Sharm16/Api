162a45c0a9d4c128fdb41830959abad74d126e01
shar <shyam-s@live.co.uk>
2018-07-25 10:58:58 +0100

    first commit

---
+++ src/main/java/com/qa/Application.java
@@ -0,0 +1,52 @@
+package com.qa;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
+
+@SpringBootApplication
+public class Application {
+	
+	static final String topicExchangeName = "spring-boot-exchange";
+
+	static final String queueName = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	}
+	
+	@Bean
+	RestTemplate getRestTemplate() 
+	{
+		return new RestTemplate();
+	}
+	
+	@Bean
+	Gson getGson() 
+	{
+		return new Gson();
+	}
+
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class).close();;
+	}
+	
+}
